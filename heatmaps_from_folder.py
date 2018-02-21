894ef7eb3d60655c8baa0f4a51ecfc680c84fb06
Aaron Stanek <nope>
2018-02-20 16:46:51 -0800

    can now plot heatmaps

--- heatmaps_from_folder.py
+++ heatmaps_from_folder.py
@@ -1,7 +1,36 @@
 from get_files_in_folder import *
 from read_json_ord_clump import *
 from integrate_json_ord_clump import *
-from generate_heatmap import *
+from generate_heatmap_2 import *
 from slice_data import *
 from compute_run_duration import *
 from background_elimination import *
+
+def heatmaps_from_folder(folder_path,what,**options):
+    # folderpath is a strings
+    # what is a dict integer -> dict(integer->list(string))
+    # the first interger is the first channel, the second integer is the second channel
+    # it maps channel numbers to [title,x_label,y_label,x_bin_count,y_bin_count,filename]
+    # what dictates details of the plot to be generated for that channel
+    # filename is the full path where the plot will be save (prefer ending in .png)
+    # this function takes the data from a folder and plots it
+    # present options nonneg = True, cuts all negative data points
+    file_list = get_files_in_folder(folder_path)
+    t = 0.0
+    event_list = []
+    for f in file_list:
+        [ev,t] = read_json_ord_clump_basic(f) # you better hope that they all have the same delta t
+        event_list = event_list + ev
+        del(ev)
+    del(file_list) # just to be nice
+    integrated = integrate_json_ord_clump(event_list,t)
+    del(event_list)
+    del(t)
+    for plotnum1 in what:
+        for plotnum2 in what[plotnum1]:
+            u = []
+            for f in integrated:
+                u.append([f[plotnum1],f[plotnum2]])
+            k = what[plotnum1][plotnum2]
+            generate_heatmap(k[0],k[1],k[2],k[3],k[4],u,k[5])
+            del(k)
