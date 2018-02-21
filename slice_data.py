311247c19a1c154328f13093df9b59fdc54124b6
Aaron Stanek <nope>
2018-02-21 14:25:46 -0800

    can now slice in higher dimensions

--- slice_data.py
+++ slice_data.py
@@ -11,3 +11,19 @@ def remove_below(raw,limit):
         if x>=limit:
             ou.append(x)
     return ou
+
+# the next two functions implement cuts for the case that we have data in more than one dimension
+
+def remove_above_MD(raw,limit,index):
+    ou = []
+    for x in raw:
+        if x[index]<=limit:
+            ou.append(x)
+    return ou
+
+def remove_below_MD(raw,limit,index):
+    ou = []
+    for x in raw:
+        if x[index]>=limit:
+            ou.append(x)
+    return ou
