2a57bc9bd37a9fc760e6b4f3dbc1fd6ed8e29e69
Aaron Stanek <nope>
2018-02-21 16:06:31 -0800

    part of heatmap with background elimination written

--- heatmaps_from_folder.py
+++ heatmaps_from_folder.py
@@ -50,3 +50,47 @@ def heatmaps_from_folder(folder_path,what,**options):
             k = what[plotnum1][plotnum2]
             generate_heatmap(k[0],k[1],k[2],k[3],k[4],u,k[5])
             del(k)
+
+def heatmaps_eliminating_background(data_folder_path,background_folder_path,what,**options):
+    # see above for argument information
+    # determine elimination type
+    elim_type = "time"
+    if options["elim_type"] == "function":
+        elim_type = "function"
+    j = [] # j will be a list of lists of events
+    dur = [] # this will hold run durations, floats
+    event_counts = []
+    for x in [data_folder_path,background_folder_path]:
+        file_list = get_files_in_folder(x)
+        t = 0.0
+        event_list = []
+        for f in file_list:
+            [ev,t] = read_json_ord_clump_basic(f)
+            event_list = event_list + ev
+            del(ev)
+        dur.append(compute_run_duration(event_list))
+        j.append(integrate_json_ord_clump(event_list,t))
+        event_counts.append(len(event_list))
+    del(file_list)
+    del(event_list)
+    del(t)
+    for plotnum1 in what:
+        for plotnum2 in what[plotnum1]:
+            u = [] # source data
+            v = [] # background data
+            for i in range(len(j[0])):
+                u.append([j[0][i][plotnum1],j[0][i][plotnum2]])
+            for i in range(len(j[1])):
+                v.append([j[1][i][plotnum1],j[1][i][plotnum2]])
+            if "remove_below_x" in options:
+                u = remove_below_MD(u,options["remove_below_x"],0)
+                v = remove_below_MD(v,options["remove_below_x"],0)
+            if "remove_above_x" in options:
+                u = remove_above_MD(u,options["remove_above_x"],0)
+                v = remove_above_MD(v,options["remove_above_x"],0)
+            if "remove_below_y" in options:
+                u = remove_below_MD(u,options["remove_below_y"],1)
+                v = remove_below_MD(v,options["remove_below_y"],1)
+            if "remove_above_y" in options:
+                u = remove_above_MD(u,options["remove_above_y"],1)
+                v = remove_above_MD(v,options["remove_above_y"],1)
