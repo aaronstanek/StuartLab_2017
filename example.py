97fb7075cc64afd5e3cbc7c63f9fc1c0c62c5d13
Aaron Stanek <nope>
2018-02-22 10:30:11 -0800

    example section 3

--- example.py
+++ example.py
@@ -59,3 +59,43 @@ histograms_from_folder("~/Desktop/my_data_folder",plot_info,remove_below=0,energ
 
 # this will plot the data from ~/Desktop/my_data_folder according to plot_info
 # AND it will remove all datapoints that fall below zero, and multiply the observed energy of the remaining datapoints by 7.44
+
+# Section 3: Eliinating the Background
+
+from histograms_from_folder import histograms_eliminating_background
+
+plot_info = {
+                1:[title,x_label,y_label,bin_count,filename],
+                2:[title,x_label,y_label,bin_count,filename],
+                3:[title,x_label,y_label,bin_count,filename],
+                4:[title,x_label,y_label,bin_count,filename]
+                }
+histograms_eliminating_background(source_folder_path,background_folder_path,plot_info)
+
+# source_folder is a directory path where the data for the run with the source is stored
+# background_folder is a directory path where the data fir the run without the source is stored
+# plot_info follows the same rules as in Section 1
+
+# options
+
+# all the options that are valid for histograms_from_folder are also valid for histograms_eliminating_background and have the same effects
+# plus there are additional options for histograms_eliminating_background
+
+# because the source of the data is uncertain, we must include multiple methods for removing the background
+# if your detector can record events as fast as they occur, do not mess with this option
+# the software will use the timestamps in the data to determine the hit rate, and use this to subtract the background, this is a very accurate method
+# if your detector records events substantially slower than they occur, we cannot use default option here
+# using the default option on data that came from a slow detector would cause the software to believe that the signal and noise have equal frequency
+# the work around is to use linear algebra to get a good enough estimate of the background
+# to use this option, elim_type="function"
+
+# so, if your detector is slow compared to the hit rate, you should use:
+histograms_eliminating_background(source_folder_path,background_folder_path,plot_info,elim_type="function")
+
+# if you use the default option, the vertical axis of your plot will show hits per hour
+# if you use elim_type="function", the vertical axis of your plot will show a probability density
+# if you want a hit count instead of a probability density, use the option use_count=True
+histograms_eliminating_background(source_folder_path,background_folder_path,plot_info,elim_type="function",use_count=True)
+
+# beware, using the default background elimination method and use_count=True has undefined behavior
+histograms_eliminating_background(source_folder_path,background_folder_path,plot_info,use_count=True) # DO NOT USE THIS LINE
