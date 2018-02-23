37b62119adbbb49dc6df0cd7f76754968e5e3934
Aaron Stanek <nope>
2018-02-22 16:18:47 -0800

    run duration is now a float, this probably won't make any new bugs

--- compute_run_duration.py
+++ compute_run_duration.py
@@ -8,4 +8,4 @@ def compute_run_duration(event_list):
     first = parse_time_string(event_list[0]["EV_META"]["TIME"]) # assumes at least one event
     last = parse_time_string(event_list[-1]["EV_META"]["TIME"])
     delta = time.mktime(last) - time.mktime(first) # this converts datatype and then subtracts
-    return delta
+    return float(delta)
